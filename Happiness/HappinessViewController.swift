ef66009da0ee12b25350c86e321812abf8db84af
jgreenb2 <jgreenb2@ford.com>
2015-06-11 10:57:16 -0400

    initial happy face demo

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by jeff greenberg on 6/11/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
